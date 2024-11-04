8bd67464262d248e3ac67c30a74a221b50dc8c8b
AdrianE111 <144351143+AdrianE111@users.noreply.github.com>
2024-11-04 16:51:59 -0600

    Add files via upload

---
+++ ProyectoFinalLenguaje/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = @"C:\Users\escal\Downloads\CadenasProyectoFinalLenguaje.txt";
+
+        try
+        {
+            string[] cadenas = File.ReadAllLines(filePath);
+            if (cadenas.Length < 10)
+            {
+                Console.WriteLine("El archivo debe contener al menos 10 cadenas.");
+                return;
+            }
+
+            MaquinaTuring maquina = new MaquinaTuring();
+
+            foreach (string cadena in cadenas)
+            {
+                string trimmedCadena = cadena.Trim();
+                if (!string.IsNullOrEmpty(trimmedCadena)) // Asegúrate de que no esté vacía
+                {
+                    maquina.ProcesarCadena(trimmedCadena);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error al leer el archivo: {ex.Message}");
+        }
+    }
+}
